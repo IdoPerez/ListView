133f6743d422719ba1d20ad2867de6409eb1b3a3
IdoPerez <ap1597@bs.amalnet.k12.il>
2019-09-18 14:35:28 +0300

    go fuck ur self

--- app/src/main/java/com/example/listview/States2.java
+++ app/src/main/java/com/example/listview/States2.java
@@ -1,13 +1,37 @@
 package com.example.listview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class States2 extends AppCompatActivity {
-
+public class States2 extends AppCompatActivity implements AdapterView.OnItemClickListener {
+ String[] states;
+ ListView list;
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_states2);
+        list = (ListView) findViewById(R.id.list);
+        back = (Button) findViewById(R.id.back);
+
+       ArrayAdapter<String> px = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, states);
+        list.setAdapter(px);
+        list.setOnItemClickListener(this);
+
+        Intent gi= getIntent();
+        switch (gi.getIntExtra("p",0)){
+            case 0:
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
